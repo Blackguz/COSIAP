cfffe11142bf98f1a205540bc5256d5b99b63531
MJavoso <marcoapdr15@gmail.com>
2023-05-05 23:20:29 -0600

    Modificada la vista para retornar la clase de css con la disposición a mostrar en la tarjeta_beca.html

--- convocatorias/views.py
+++ convocatorias/views.py
@@ -7,6 +7,16 @@ from .models import Modalidad
 def obtener_becas(limite: int) -> list[Modalidad]:
     return Modalidad.objects.all()[:limite]
 
+def obtener_disposicion(tamano_becas: int) -> str:
+    return "" if tamano_becas >= 3 else "gdisp2" if tamano_becas == 2 else "gdisp1"
+def procesar_becas() -> dict:
+    diccionario_becas = {
+        "becas": {
+            "becas": (becas := obtener_becas(3)),
+            "disposicion": obtener_disposicion(len(becas))
+        }
+    }
+    return diccionario_becas
 def index(request):
     if request.method == 'POST':
         formulario_solicitud = SolicitudApoyoForm(request.POST)
@@ -15,7 +25,7 @@ def index(request):
             return redirect('index')
     else:
         formulario_solicitud = SolicitudApoyoForm()
-    return render(request, 'index.html', {'form': formulario_solicitud, "becas": obtener_becas(3)})
+    return render(request, 'index.html', {'form': formulario_solicitud, **procesar_becas()})
 
 def solicitud_de_apoyos(request):
     return render(request, 'solicitud_apoyo.html')
