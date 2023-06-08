983cfbd92e53faf6af887645fce22332b31337ba
GaelGC04 <garciajorgecf@gmail.com>
2023-06-07 22:50:29 -0600

    Añadiendo formularios dinámicos a solicitud de apoyos

--- convocatorias/views.py
+++ convocatorias/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from soporte.forms import SolicitudApoyoForm
 from .utils import procesar_becas
-from .models import Modalidad, Formulario
+from .models import Modalidad, Formulario, AtributosFormulario
+from convocatorias.forms import AtributoFormularioForm
 
 # Create your views here.
 
@@ -16,7 +17,15 @@ def index(request):
     return render(request, 'index.html', {'form': formulario_solicitud, **procesar_becas()})
 
 def solicitud_de_apoyos(request, id):
-    modalidad = get_object_or_404(Modalidad, pk=id)
-    formulario = get_object_or_404(Formulario, pk=modalidad.nombre)
-    return render(request, 'solicitud_apoyo.html', {'modalidad': modalidad, 'formulario':formulario})
+    if request.method == 'POST':
+        atributo = "Añadir documento"
+        id_formulario = request.POST["id_formulario"]
+        atributosFormulario = AtributosFormulario.objects.filter(id_formulario=id_formulario)
+        arregloAtributos = []
+        return redirect("/solicitud_de_apoyos/6")
+    else:
+        modalidad = get_object_or_404(Modalidad, pk=id)
+        formulario = get_object_or_404(Formulario, pk=id)
+        atributosFormulario = AtributosFormulario.objects.filter(id_formulario=formulario.pk)
 
+        return render(request, 'solicitud_apoyo.html', {'modalidad': modalidad, 'formulario':formulario, 'atributos':atributosFormulario})
